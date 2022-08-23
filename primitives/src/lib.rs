9a3bd323473314beb8b31d7dc81cdf982b2c72fb
andrewmah <andrewmah888@gmail.com>
2022-08-23 14:36:29 -0400

    Add PRF

--- primitives/src/lib.rs
+++ primitives/src/lib.rs
@@ -2,8 +2,8 @@ use utils::exp;
 
 //one way function (OWF)
 fn base_one_way_fn(x: u128) -> u128 {
-    let p: u128 = 98784247763; //prime mod
-    let g: u128 = 1313897859; //generator of prime order (2^31 - 1) subgroup
+    let p: u128 = 2147483783; //prime mod
+    let g: u128 = 2; //generates group of order 1073741891 
 
     return exp(g, x, p);
 }
@@ -14,7 +14,7 @@ pub fn one_way_fn(x: u128) -> u128 {
     let y1 = base_one_way_fn(x >> 64);
     let y2 = ((x) as u64) as u128;
 
-    return (y1) << 64 | y2;
+    return (y1 << 64) | y2;
 }
 
 
@@ -49,10 +49,26 @@ pub fn prg(x: u128) -> (u128, u128) {
         g1 = g1 << 1;
         g1 = g1 | hcp(s);
     }
-    for _ in 0..128 {
+    for _i in 0..128 {
         s = one_way_fn(s);
         g2 = g2 << 1;
         g2 = g2 | hcp(s);
     }
     return (g1, g2);
+}
+
+//pseudo random function (PRF)
+pub fn prf(key: u128, x: u128) -> u128 {
+    let mut output = x;
+    let mut key_bits = key;
+    for _i in 0..128 {
+        let (g0, g1) = prg(output);
+        if (key_bits & 0x1 == 0) {
+            output = g0;
+        }
+        else {
+            output = g1;
+        }
+    }
+    return output;
 }
