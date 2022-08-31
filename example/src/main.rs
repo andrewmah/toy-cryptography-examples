793a51e12915707e13727ea4c2bfef8048cfb49b
andrewmah <andrewmah888@gmail.com>
2022-08-31 12:51:06 -0400

    Allow user input

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,23 +1,22 @@
-use rand::Rng;
-
-use primitives;
-//use utils;
-
+use encryption;
 
 fn main() {
+    let secret_key = encryption::gen();
+    println!("secret key: {:x}\n", secret_key);
 
-    for _i in 1..20 {
-        //println!("Hello world");
-        let rnd_tmp = rand::thread_rng().gen_range(0..(1 << 100 - 1));
-        //let fn_result = primitives::one_way_fn(rnd_tmp) >> 64;
-        //let hcp_result = primitives::hcp(rnd_tmp);
-        //println!("f({0}) = {1}, hcp(f({0})) = {2}", rnd_tmp, fn_result, hcp_result);
+    loop {
+        let mut message = String::new();
+        println!("Enter a message: ");
+        std::io::stdin().read_line(&mut message).unwrap();
+        print!("\nmessage: {}", message);
 
-        let (g1, g2) = primitives::prg(rnd_tmp);
-        println!("x = {0}, G(x) = {1}, {2}", rnd_tmp, g1, g2);
-        //println!("x = {0:#b}, G(x) = {1:#b}{2:#b}", rnd_tmp, g1, g2);
+        let (ciphertext, initial_vector) = encryption::enc(secret_key, &message);
+        println!("ciphertext: ");
+        for c in &ciphertext {
+            println!("    {:x}, ", c);
+        }
+
+        let plaintext = encryption::dec(secret_key, &ciphertext, initial_vector);
+        println!("decrypted plaintext: {}", plaintext);
     }
-
-
-    
 }
